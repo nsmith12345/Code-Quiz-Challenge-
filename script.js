24d2606492ddb75184c6cadef164874abbf37712
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-19 17:10:26 -0400

    Global Variables

--- script.js
+++ script.js
@@ -19,3 +19,10 @@ var cButton = document.getElementById("c"); //C Button Response
 var dButton = document.getElementById("d"); //D Button Response
 var userInitialInput = document.getElementById("userInits");
 
+//Quiz Global Variables
+var questonNumber = 0; //Current Question
+var timeRemaining = 70; //How much time is remaining - also user score
+var score = 0; //Takes the value of time remaining at completion and stores it
+var timing = 0; //Interval timer, initially set to 0
+var userName = ""; //Stores the user name or initials
+
