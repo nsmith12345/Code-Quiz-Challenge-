9f0dce030fadb0e4b9b85d456665a3d981a82cf8
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-19 17:07:04 -0400

    DOM Variables

--- script.js
+++ script.js
@@ -1,73 +1,21 @@
 // Assignment code here
 
-// Link start button to timer and start quiz questions
-document.createElement("button")
+// ******************* Variables *****************************//
+// DOM Variables
+var topNavContainer = document.getElementById("pageTopNav"); // Top Navigation - includes time, and view scores
+var timeTracker = document.getElementById("time"); //Store time
+var viewScoresLink = document.getElementById("viewScores"); //Shows the "view scores" at the top of each page
+var startPageContainer = gocument.getElementById
+var questionPageContainer = document.getElementById("questionPage"); //Question Page Container
+var questionTitleContainer = document.getElementById("QuestionTitle"); //Stores the Question Title
+var resultsPageContainer = document.getElementById("results"); //Results Page
+var highScoresPageContainer = document.getElementById("highScores"); //High score page
+var showHighScores = document.getElementById("highestScore"); //Filed for the highest scores
+var isCorrectContainer = document.getElementById("iscorrectField"); //Shows Wrong or Correct
+var usersScore = document.getElementById("yourScore"); //Displays the users final score at the end
+var aButton = document.getElementById("a"); //A Button Response
+var bButton = document.getElementById("b"); //B Button Response
+var cButton = document.getElementById("c"); //C Button Response
+var dButton = document.getElementById("d"); //D Button Response
+var userInitialInput = document.getElementById("userInits");
 
-let btn = document.createElement("button");
-btn.innerhtml = "Start Quiz";
-btn.onclick = function () {
-    alert("Button is clicked")
-};
-document.body.appendChild(btn);
-
-var questions = [{
-    question: "1. Commonly used data types DO NOT include?",
-    one: "strings",
-    2. "booleans",
-    3. "alerts",
-    4. "numbers",
-    correctAnswer "3"},
-{
-    question: "2. The condition in an if/else statement is enclosed with ________?",
-    1. "quotes",
-    2. "curly brackets",
-    3. "parenthesis",
-    4. "square brackets",
-    correctAnswer: "3"},
-{
-    question: "3. Arrays in JavaScript can be used to store_________?",
-    1. "numbers and strings",
-    2. "other arrays",
-    3. "booleans",
-    4. "all of the above",
-    correctAnswer: "4"},
-{
-    question: "4. String values must be enclosed within _________?",
-    1. "commas",
-    2. "curly brackets",
-    3. "quotes",
-    4. "parenthesis",
-    correctAnswer: "1"},
-{            
-    question: "5. A very useful tool used during development and debugging for printing content to the debugger is?",
-    1. "JavaScript",
-    2. "terminal/bash",
-    3. "for loops",
-    4. "console.log",
-    correctAnswer: "4"},
-];
-
-for(var i=0; i < questions.length; i++) 
-    var response = window.prompt(questions[i].prompt);
-    if(response == questions[i].answer){
-        score++;
-        alert("Correct!");
-    }   else {
-        alert("WRONG!");
-    }    
-    alert("you got " + score + "/" + questions.length);
-
-// countdown timer function here
-
-function startTimer() {
-    var counter = 60;
-    setInterval(function() {
-        counter--;
-        if (counter >= 0) {
-            span = document.getElementById("count");
-            span.innerHTML = counter;
-        }
-        if (counter === 0) {
-            alert
-        }
-    }
