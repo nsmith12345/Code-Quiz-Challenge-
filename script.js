2db0659e1922373223d811c3393c523da444e985
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-20 10:54:26 -0400

    JavaScript addition

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ var questions = [
     "correct": "c"
     },
     {"Question": "Arrays in JavaScript can be used to store_________?"
-    "numbers and stringsA": numbers and strings,
+    "numbersandstringsA": numbers and strings,
     "other arraysB": other arrays,
     "booleansC": booleans,
     "all of the above D": all of the above,
@@ -59,7 +59,7 @@ var questions = [
     {"Question": "A very useful tool used during development and debugging for printing content to the debugger is?"
     "JavaScriptA": JavaScript,
     "terminal/bashB": terminal/bash,
-    "for loopsC": for loops,
+    "for loopsC": forloops,
     "console.logD": console.log,
     "correct": "d"
     },
@@ -80,9 +80,88 @@ function welcome () {
     isCorrectContainer.style.display="none";
 }
 
+//Display Question FUnction - Displays the Question Page for Current Question Number
+function displayQuestion() {
+    console.log("display Question");
+    if(questionNumber == 0) {counDown();}
+    questionNumber +=1;
+    questionPageContainer.style.display="block";
+    startPageContainer.style.display="none";
+    if ((questionNumber <= questions.length) && (questionNumber !=0)) { //Checks to see if the question number is between 1 and the number of questions
+        
+    console.log ("Asking a question:");
+    questionTitleContainer.textContent = questions[questionNumber -1].Question; //Displays the title of the question
+    aButton.textContent = questions[questionNumber - 1].answerA; // Sets Button A to the first choice
+    bButton.textContent = questions[questionNumber - 1].answerB; // Sets Button B to the first choice    
+    cButton.textContent = questions[questionNumber - 1].answerC; // Sets Button C to the first choice     
+    dButton.textContent = questions[questionNumber - 1].answerD; // Sets Button D to the first choice
+    } else { //Ends the quiz if the last question was reached
+    console.log("Quiz is over");
+    endQuiz();
+    }
+    
+}
 
+//Check Answer Function - Checks the Answer
+function checkAnswer(returnValue) {
+    isCorrectContainer.style.display = "block"; //Makes the container at the bottom is visible
+    console.log("Checking Answer");
+    if (returnValue == questions[questionNumber - 1].correct) { //takes the return value (from the button pressed) and checks to see if it is the correct answer
+    console.log("Correct Answer");
+    displayQuestion();
+    isCorrectContainer.textContent = "Correct!";
 
+    } else {
+        timeRemaining -=10;
+        displayQuestion();
+        isCorrectContainer.textContent = "Wrong!";
+    }
+    console.log(timeRemaining);
+    }
 
+// High Score Function - Displays the High Score Page
+function highScore() {
+
+    resultsPageContainer.style.display = "none";
+    highScoresPageContainer.style.display = "block";
+    isCorrectContainer.style.display = "none";
+    userName = userInitialInput.ariaValueMax; //Takes the initials form the textfield on the High Score page
+    console.log(userName);
+    console.log("Score: " + score);
+
+}    
+
+//Results - Determines the Results - only if time is out or last question reached
+function endQuiz() {
+    clearInterval(timing); //Stops the timer
+    score = timeRemaining; //Sets the score to the current time remaining
+    timeTracker.style.display = "none"; //Hides the time countdown
+    questionPageContainer.style.display="none"; //Hides the question page
+    resultsPageContainer.style.display="block"; //Shows the results page
+    usersScore.textContent = "Your final score is: " + score; //Shows the user their score 
+}
+
+//Clear High Score - Clears the high scores list
+function clearHigh() {
+//* This will hold how to clear the high scores list */
+
+}
+
+//Controls the countdown
+function countDown() {
+    
+    console.log("Timer Started");
+    timing = setInterval(function() {
+
+        timeRemainging--;
+        timeTracker.textContent = "Time left: " + timeRemaining; //Displays the timer as each tick cycles
+
+        if(timeRemaining === 0) { // if time remaining is 0 then clear the interval and go to End
+        clearInterval(timing);
+        endQuiz();
+        }
+    }, 1000); //1000 is the interval for a second
+}
 
 /***************************** START EXECUTION ON PAGE LOAD *********************************/
 console.log("Javascript loaded");
